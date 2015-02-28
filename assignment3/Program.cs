7ccefcc6c74dd7af717f0560b4b178730c6eca2f
HanChengLiang <James_61111@live.ca>
2015-02-28 00:11:44 -0500

    added calculations

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -10,6 +10,13 @@ namespace Abstract_Planet
     {
         static void Main(string[] args)
         {   
+            //caculation of mars
+            GiantPlanet gianPlanet = new GiantPlanet("Mars", 639E21, 6779, "Terrestrial");
+            Console.WriteLine(gianPlanet.ToString());
+
+            //calculation of earth
+            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 5.972E24, 12742, true);
+            Console.WriteLine(terrestrialPlanet.ToString());
 
         }
     }
